3eda6d14b6fd700ca542f3c535a8e7a7f9481b9d
Lucas Lisboa <lucas@pop-os.localdomain>
2022-02-18 18:00:03 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,2 +1,17 @@
-test('', () => {
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testando o componente "NotFound"', () => {
+  test('Se o componente exibe a mensagem "Page requested not found"', () => {
+    render(<NotFound />);
+    const notFoundText = screen.getByText(/Page requested not found/);
+    expect(notFoundText).toBeInTheDocument();
+  });
+  test('Se foi renderizada a imagem correta', () => {
+    render(<NotFound />);
+    const gif = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByRole('img', { name: /Pikachu/i });
+    expect(image.src).toBe(gif);
+  });
 });
